8e8cbd5403f0d1f996b987dfe22c7d90461920e5
randomprogramming <isnipedodac@gmail.com>
2020-11-21 21:34:08 +0100

    added custom font to project

--- App.js
+++ App.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import {SafeAreaView, View, Text, StatusBar} from 'react-native';
+import {SafeAreaView, View, Text, StatusBar, StyleSheet} from 'react-native';
+import {palette} from './theme';
 
 const App = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <View>
-          <Text>Hello world</Text>
-        </View>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={palette.backgroundDark}
+      />
+      <SafeAreaView style={styles.main}>
+        <Text
+          style={{
+            color: '#fff',
+            fontFamily: 'Poppins-SemiBold',
+            fontSize: 20,
+          }}>
+          Lorem Ipsum Dolor Amet
+        </Text>
       </SafeAreaView>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: palette.backgroundDark,
+  },
+});
 
 export default App;
