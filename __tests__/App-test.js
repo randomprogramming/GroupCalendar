2adc38967620550eb79423394fc1ad257af49fd1
randomprogramming <isnipedodac@gmail.com>
2020-12-09 12:52:51 +0100

    basic testing

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -8,7 +8,17 @@ import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import {Provider} from 'react-redux';
+import {applyMiddleware, createStore} from 'redux';
+import rootReducer from '../src/reducers/rootReducer';
+import thunk from 'redux-thunk';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  renderer.create(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
 });
